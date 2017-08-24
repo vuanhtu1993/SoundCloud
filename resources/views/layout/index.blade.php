415cfb10a07db881caa7b04596f28670b7989ebb
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-24 15:58:47 +0700

    add Modal bootstrap

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -37,7 +37,58 @@
                 </div>
             </div>
         </div>
+        <!-- Modal -->
+        <div class="modal fade" tabindex="-1" role="dialog" id="myModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                    aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Modal title</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form name="frmSinhVien" class="form-horizontal">
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-3 control-label">Họ tên</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="name" name="name"
+                                           placeholder="Vui lòng nhập họ tên"/>
+                                    <span id="helpBlock2" class="help-block">Vui lòng nhập họ tên</span>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-3 control-label">Tuổi</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="age" name="age"
+                                           placeholder="Vui lòng nhập tuổi">
+                                    <span id="helpBlock2" class="help-block">Vui lòng nhập tuổi</span>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-3 control-label">Email</label>
+                                <div class="col-sm-9">
+                                    <input type="email" class="form-control" id="email" name="email"
+                                           placeholder="Vui lòng nhập Email"/>
+                                    <span id="helpBlock2" class="help-block">Vui lòng nhập email</span>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-3 control-label">Điện thoại</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="phone" name="phone"
+                                           placeholder="Vui lòng nhập số điện thoại"/>
+                                    <span id="helpBlock2" class="help-block">Vui lòng nhập điện thoại</span>
+                                </div>
+                            </div>
 
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary">Lưu</button>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
     </div>
 </div>
 
